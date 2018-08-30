ca7c8fb6ef661a21fbe8c6272971b37f70703193
In Seop Hwang <stevehwang4147@gmail.com>
2018-08-30 00:14:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ if (name === "Susan") {
 // Don't worry about this
 module.exports = { name, height
 }
-
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
